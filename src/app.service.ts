9263c8ed26ece143da9b50ad94f15a40944344ca
RysanDeluna <82891214+RysanDeluna@users.noreply.github.com>
2022-10-14 23:04:27 -0300

    implementação do filtro

--- src/app.service.ts
+++ src/app.service.ts
@@ -44,7 +44,7 @@ const sampleData: Array<Empresa> = [emp, emp2, emp3];
 @Injectable()
 export class AppService {
   getEmpresas(name: string, location: string): Array<Empresa> {
-    let results: Array<Empresa> = [];
+    let results: Array<Empresa> = [...sampleData];  // Copia sample para results
     console.log(sampleData.length)
     if (!name && !location){
       return sampleData;
@@ -56,57 +56,28 @@ export class AppService {
         location = location.replace(/^"(.+(?="$))"$/, '$1');
         var obj_location = JSON.parse(location); // transforma location em objeto
       }
-      results = sampleData;
+
       let len = results.length;
+      let flag: Boolean = false;
 
       for (let i = 0; i < len; i++) {
-        if (name && results[i].name != name) results.splice(i, 1);
+        flag = false;
+        if (name && results[i].name != name) flag = true;
         else if (obj_location)
+        {
           if (obj_location.cidade && results[i].location.cidade != obj_location.cidade)
-            results.splice(i, 1);
+            flag = true;
           else if (obj_location.bairro && results[i].location.bairro != obj_location.bairro)
-            results.splice(i, 1);
+            flag = true;
           else if (obj_location.estado && results[i].location.estado != obj_location.estado)
-            results.splice(i, 1);
+            flag = true;
+        }
+        if (flag == true) {
+          results.splice(i, 1);
+          len--; i--;
+        }
       }
     }
     return results;
-    // else {
-    //   if(name){
-    //     name = name.replace(/^"(.+(?="$))"$/, '$1');
-    //     for (let i = 0;i < sampleData.length; i++) {
-    //       console.log(i)
-    //       if (sampleData[i].name.includes(name)){
-    //         results.push(sampleData[i])
-    //       }
-    //     }
-    //     if(location){
-    //       location = location.replace(/^"(.+(?="$))"$/, '$1');
-    //       console.log(location);
-    //       var json_location = JSON.parse(location);
-    //       for (let i = 0; i < results.length; i++){
-    //         console.log(i);
-    //         if (results[i].location.estado.includes(json_location.estado)){
-    //           results.push(results[i]);
-    //         }
-    //       }
-    //     }
-    //   } else {
-    //     if(location){
-    //       location = location.replace(/^"(.+(?="$))"$/, '$1');
-    //       console.log(location);
-    //       var json_location = JSON.parse(location);
-    //       for (let i = 0; i < sampleData.length; i++){
-    //         console.log(i);
-    //         if (sampleData[i].location.estado.includes(json_location.estado)){
-    //           results.push(sampleData[i]);
-    //         }
-    //         // falta definir para cidade e bairro
-    //       }
-    //     }
-    //   }
-    //   return results;
-    // }
   }
 }
-
