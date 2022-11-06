f6d1311bb2f4de25062b728ab3b87fa90af8cb07
Vinicius Leite <87040226+ViniciusLeitedoCarmo@users.noreply.github.com>
2022-11-06 17:40:48 -0300

    Adding imports.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Headers, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Empresa } from './Empresa/empresa.model';
 
